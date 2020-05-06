b0b28b16b35fa1f7a8ee68a75f701b6020c32da8
intinazca <60673220+intinazca@users.noreply.github.com>
2020-05-06 13:03:20 -0500

    personaje
    Se añadio el personaje del cerdo, se hizo una animacion de salto parecida a la del juego original de CrossyRoad aplicada a un cubo.

---
+++ Assets/Scrips/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator;
+    private bool isHopping;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && !isHopping)
+        {
+            animator.SetTrigger("Hop");
+            isHopping = true;
+        }
+    }
+
+    public void FinishHop()
+    {
+        isHopping = false;
+    }
+}
+
--- Assets/Scrips/TerrainData.cs
+++ Assets/Scrips/TerrainData.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Terrain Data", menuName = "Terrain Data")]
 public class TerrainData : ScriptableObject
 {
-
     public GameObject terrain;
     public int maxInSuccession;
-
 
 }
